cd4f7caaeb7511627517a0f80629e602fe30ce13
Andrei Haiu <andrei.haiu@htss.ro>
2022-05-08 16:46:05 +0300

    deploy

---
+++ precache-manifest.33af58fcff27add4c283be58831595d0.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ec38a92fa774e5530d",
+    "url": "/letmewear/css/app.40253cb2.css"
+  },
+  {
+    "revision": "a8ec38a92fa774e5530d",
+    "url": "/letmewear/js/app.86266af3.js"
+  },
+  {
+    "revision": "2c4d91dc823676d6c5c4",
+    "url": "/letmewear/js/chunk-52e4de86.a2cbec5d.js"
+  },
+  {
+    "revision": "9abc6688eefa39c62cc9",
+    "url": "/letmewear/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "9abc6688eefa39c62cc9",
+    "url": "/letmewear/js/chunk-ebdaf33a.c74fa198.js"
+  },
+  {
+    "revision": "22ad6411a342490293f9",
+    "url": "/letmewear/js/chunk-vendors.dec0a818.js"
+  },
+  {
+    "revision": "b7269f5fc9792e2c9298cb3ba3882f41",
+    "url": "/letmewear/img/play.b7269f5f.png"
+  },
+  {
+    "revision": "0d06490b80a75428b9c9ebb04a1858ab",
+    "url": "/letmewear/img/four.0d06490b.png"
+  },
+  {
+    "revision": "d61555e4039af64e387dc76b1963c331",
+    "url": "/letmewear/img/second.d61555e4.svg"
+  },
+  {
+    "revision": "b31db6aaa3f3cd753c621e9764ae0ab1",
+    "url": "/letmewear/img/quality.b31db6aa.svg"
+  },
+  {
+    "revision": "cbd2cbd7513302c142f068c68fab97a4",
+    "url": "/letmewear/img/two.cbd2cbd7.svg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/letmewear/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/letmewear/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/letmewear/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/letmewear/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/letmewear/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "046a034c7b8c62d4d5f69cfff5967cc6",
+    "url": "/letmewear/img/welcome.046a034c.png"
+  },
+  {
+    "revision": "e007223564247334fa2f27f9ed226f13",
+    "url": "/letmewear/media/video full cut.e0072235.mp4"
+  },
+  {
+    "revision": "2366d1ce27b44b383841320872ee478d",
+    "url": "/letmewear/img/three.2366d1ce.svg"
+  },
+  {
+    "revision": "5bf78f8735c5082df9d81a3cbcb5cfb7",
+    "url": "/letmewear/img/ios.5bf78f87.png"
+  },
+  {
+    "revision": "0377f32ba3cf961bda203d0b3d6823fc",
+    "url": "/letmewear/img/one.0377f32b.svg"
+  },
+  {
+    "revision": "282b0ff4e0d8ef3f538a2ec8adbaba44",
+    "url": "/letmewear/img/dashboard.282b0ff4.jpeg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/letmewear/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/letmewear/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/letmewear/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/letmewear/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/letmewear/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "a8467997b8b11942c899a8c343a3cab1",
+    "url": "/letmewear/img/third.a8467997.svg"
+  },
+  {
+    "revision": "24e2b4a12a452746e09c01b4c0a933d3",
+    "url": "/letmewear/img/first.24e2b4a1.svg"
+  },
+  {
+    "revision": "d5fcf21e9c101278d66f96abc0dde262",
+    "url": "/letmewear/index.html"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/letmewear/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/letmewear/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/letmewear/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/letmewear/img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/letmewear/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/letmewear/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/letmewear/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fb439881611db4497eafe08c65a17f0d",
+    "url": "/letmewear/img/theme/adina.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/letmewear/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/letmewear/img/theme/profile.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/letmewear/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/letmewear/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/letmewear/img/theme/promo-1.png"
+  },
+  {
+    "revision": "a50bada3d5692533fb657d4a596170c0",
+    "url": "/letmewear/img/theme/stef.jpg"
+  },
+  {
+    "revision": "754f3f53534325e00cc6766f0856dec6",
+    "url": "/letmewear/img/theme/mobile.svg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/letmewear/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/letmewear/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/letmewear/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/letmewear/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "0a7304a95787cb619d98a3540899bb67",
+    "url": "/letmewear/img/theme/ghili.jpg"
+  },
+  {
+    "revision": "1d913f772fceafc34b11ecaa849e6779",
+    "url": "/letmewear/img/theme/toni.jpg"
+  }
+];
